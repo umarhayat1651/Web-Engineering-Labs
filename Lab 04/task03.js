41deec9cce80f154573e081527b4e9ff2cca4b62
umarhayat <umarhayat1651@gmail.com>
2026-03-01 00:50:40 +0500

    First 3 Lab Tasks

--- Lab 04/task03.js
+++ Lab 04/task03.js
@@ -1,1 +1,5 @@
-document.write("<br>x = y + 1 ...Result = 3<br>x += y ...Result = 6<br>x -= y ...Result = 3<br>x *= y ...Result = 9<br>x /= y ...Result = 3")
+document.write("<br>x = y + 1 ...Result = 3");
+document.write("<br>x += y ...Result = 6");
+document.write("<br>x -= y ...Result = 3");
+document.write("<br>x *= y ...Result = 9");
+document.write("<br>x /= y ...Result = 3")
